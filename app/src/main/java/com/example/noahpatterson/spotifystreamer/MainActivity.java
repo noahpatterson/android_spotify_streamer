47f9de2a8379fe296d66d6266af07d939e43011a
noahpatterson <n@noahpatterson.me>
2015-08-17 20:18:36 -0400

    Give fragments unique ids

--- app/src/main/java/com/example/noahpatterson/spotifystreamer/MainActivity.java
+++ app/src/main/java/com/example/noahpatterson/spotifystreamer/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.noahpatterson.spotifystreamer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("main_activity", "in onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("main_activity", "in onRestoreInstanceState");
     }
 }
